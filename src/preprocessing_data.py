57a21aadbcebb7ffdb16d9707fd898d0fa918f76
Aung Kaung <aungkaung855@gmail.com>
2025-11-07 21:02:41 +0000

    preprocessing is almost done a little more work

--- src/preprocessing_data.py
+++ src/preprocessing_data.py
@@ -9,10 +9,21 @@ import os
 import matplotlib.pyplot as plt
 
 
+# convert audio data into model's expected input
+def convert_to_input(y, target_length):
+    if len(y) < target_length:
+        y = np.pad(y, (0, target_length - len(y)))
+    else:
+        y = y[:target_length]
+        return y
+
+
 # resample audio function
 def resample_audio(full_path, target_sr=16000):
     y, sr = librosa.load(full_path, sr=target_sr)
     return y, sr
+
+# low pass filter function
 
 
 def low_pass_filter(data, cutoff_freq, sample_rate, order=4):
@@ -31,6 +42,9 @@ for root, dirs, files in os.walk(master_path):
         if file.endswith((".wav")):
             full_path = os.path.join(root, file)
             y, sr = resample_audio(full_path)
-            print(f"{file} >>> resampled at {sr}")
+            print(f"{file} >>> resampled at {sr}Hz")
 
 filtered_audio = low_pass_filter(y, cutoff_freq=4000, sample_rate=sr)
+
+model_input = convert_to_input(filtered_audio, target_length=16000)
+print(f"Model input shape: {model_input.shape}")
