e6297497d8a20489236a6214a31297e3dfcc1b6b
Aung Kaung <aungkaung855@gmail.com>
2025-11-20 16:29:29 +0000

    models.py working iteration

---
+++ src/models.py
@@ -0,0 +1,38 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class AudioCNN(nn.Module):
+    def __init__(self, num_classes):
+        super(AudioCNN, self).__init__()
+
+        self.conv1 = nn.Conv2d(1, 16, kernel_size=3, padding=1)
+
+        self.pool = nn.MaxPool2d(2, 2)
+
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, padding=1)
+
+        self.fc1 = nn.Linear(32 * 10 * 125, 128)
+        self.fc2 = nn.Linear(128, num_classes)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = self.pool(x)
+
+        x = F.relu(self.conv2(x))
+        x = self.pool(x)
+
+        x = x.view(x.size(0), -1)   # flatten
+
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+
+        return x
+
+
+def create_model(num_classes):
+    return AudioCNN(num_classes)
+
+
+print("torch v.:", torch.__version__)
